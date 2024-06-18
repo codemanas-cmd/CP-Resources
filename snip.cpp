d54e918674568dc51fa1e6d4d6bd7ed8280e7689
codemanas-cmd <manasmishra2003lm@gmail.com>
2024-06-18 19:27:32 +0530

    Add Segment Tree

--- snip.cpp
+++ snip.cpp
@@ -57,3 +57,45 @@ int GCD (int a,int b){
     }
     return b;
 }
+
+
+
+
+// ==================================== Segment-Tree ===========================================================
+
+vector<int> build(vector<int>&arr,int n){
+    vector<int> t(2*n);
+    for(int i =0;i<n;i++){
+        t[n+i] = arr[i];
+    }
+
+    for(int i = n-1;i>=1;i--){
+        t[i] = t[2*i]+t[2*i+1];
+    }
+    return t;
+}
+
+void update(vector<int>&t,int n, int idx,int new_val){
+    //idx is index in og array
+    int v = idx+n;
+    t[v]= new_val;
+    v/=2;
+    while(v>=1){
+        t[v]=t[2*v]+t[2*v+1];
+        v/=2;
+    }
+
+}
+
+int calc(vector<int>&t,int n, int l , int r){
+    int vl = l+n;
+    int vr = r+n;
+    int sum = 0;
+    while(vl<=vr){
+        if((vl&1)) sum+=t[vl++];
+        if(!(vr&1)) sum+=t[vr--];
+
+        vl/=2; vr/= 2;
+    }
+    return sum;
+}
