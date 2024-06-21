d7d7953b2b44ce558b5fd93425099647ce50bcdb
codemanas-cmd <manasmishra2003lm@gmail.com>
2024-06-21 20:06:50 +0530

    Add base code

---
+++ base.cpp
@@ -0,0 +1,69 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+// ================================== Macros ======================================================
+#define int long long
+#define vi vector<int>
+#define vb vector<bool>
+#define vs vector<string>
+#define pb push_back
+#define sz(v) = (int) v.size()
+#define mx(v) *max_element(v.begin(),v.end())
+#define mn(v) *min_element(v.begin(),v.end())
+
+int MOD=1e9+7; 
+
+void modadd(int &a , int b) {a=((a%MOD)+(b%MOD))%MOD;}
+void modsub(int &a , int b) {a=((a%MOD)-(b%MOD)+MOD)%MOD;}
+void modmul(int &a , int b) {a=((a%MOD)*(b%MOD))%MOD;}
+
+
+// ================================== take ip/op like vector,pairs directly!==================================
+template<typename typC,typename typD> istream &operator>>(istream &cin,pair<typC,typD> &a) { return cin>>a.first>>a.second; }
+template<typename typC> istream &operator>>(istream &cin,vector<typC> &a) { for (auto &x:a) cin>>x; return cin; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const pair<typC,typD> &a) { return cout<<a.first<<' '<<a.second; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const vector<pair<typC,typD>> &a) { for (auto &x:a) cout<<x<<'\n'; return cout; }
+template<typename typC> ostream &operator<<(ostream &cout,const vector<typC> &a) { int n=a.size(); if (!n) return cout; cout<<a[0]; for (int i=1; i<n; i++) cout<<' '<<a[i]; return cout; }
+// ===================================END Of the input module ==========================================
+
+
+
+void solve() {
+    
+}
+
+
+int32_t main()
+{
+ 
+ ios_base::sync_with_stdio(false);
+ cin.tie(NULL);
+
+    int T = 1;
+    cin >> T;
+    while (T--)
+    {
+        solve();
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
+void solve(){
+    int n=1,m=0;
+    string s;
+    cin>>n;
+    vi v(n);
+    cin>>v;
+    
+}
+
+
+
+    
