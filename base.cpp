a9df6dfe57e40e568f30d52415073d439e6c5390
codemanas-cmd <manasmishra2003lm@gmail.com>
2025-03-19 22:59:32 +0530

    New base snippet

--- base.cpp
+++ base.cpp
@@ -1,84 +1,31 @@
 #include "bits/stdc++.h"
 using namespace std;
-
-// ================================== Macros ======================================================
-#define int long long int
-#define uint unsigned long long
-#define ld long double
-#define vi vector<int>
-#define vb vector<bool>
-#define vs vector<string>
-#define vvi vector<vector<int>>
-#define vvb vector<vector<bool>
-#define pi pair<int,int>
-#define vpi vector<pair<int,int>>
-#define pb push_back
-#define ff first
-#define ss second
-#define el(a,v) for(auto& a : v)
-#define fr(i,a,n) for(int i=(a);i<n;i++)
-#define rf(i,n) for(int i = (n)-1;i>=0;i++)
-#define sz(v) = (int) v.size()
+#ifndef ONLINE_JUDGE
+#include "debug.h"
+#else
+#define debug(...) 8
+#endif
+#define int long long
 #define all(v) v.begin(),v.end()
-#define rev(v) reverse(v.begin(),v.end())
-#define mxe(v) *max_element(v.begin(),v.end())
-#define mne(v) *min_element(v.begin(),v.end())
-#define srt(v) sort(v.begin(),v.end())
-#define rsrt(v) sort(v.rbegin(),v.rend())
-#define dbg(var) cout<<#var<<" = "<<var<<nl;
-#define unq(v)  v.resize(distance(v.begin(), unique(v.begin(), v.end())))
-#define nl "\n"
-#define ws " "
-
-
-const int  MOD=1e9+7; 
-const int INF = INT32_MAX;
-
-int modexp(int x , int n){
-    if(n==0) return 1%MOD;
-    int u = modexp(x,n/2);
-    u = (u*u)%MOD;
-    if(n&1) u *= x;
-    return u%MOD;
-}
-
-void modadd(int &a,int b){ a= ( ( (a%MOD) + (b%MOD) ) % MOD );}
-void modsub(int &a,int b){ a = ( ( (a%MOD) - (b%MOD) + MOD ) % MOD );}
-void modmul(int &a,int b){a = ( ( (a%MOD) * (b%MOD) ) % MOD );}
-void moddiv(int &a,int b){ modmul(a , modexp(b,MOD-2 ));}
-int modinv(int y){return modexp(y,MOD-2);}
-
-void yes(){cout<<"YES"<<nl;}
-void no(){cout<<"NO"<<nl;}
-
-// ================================== take ip/op like vector,pairs directly!==================================
-template<typename typC,typename typD> istream &operator>>(istream &cin,pair<typC,typD> &a) { return cin>>a.first>>a.second; }
-template<typename typC> istream &operator>>(istream &cin,vector<typC> &a) { for (auto &x:a) cin>>x; return cin; }
-template<typename typC,typename typD> ostream &operator<<(ostream &cout,const pair<typC,typD> &a) { return cout<<a.first<<' '<<a.second; }
-template<typename typC,typename typD> ostream &operator<<(ostream &cout,const vector<pair<typC,typD>> &a) { for (auto &x:a) cout<<x<<'\n'; return cout; }
-template<typename typC> ostream &operator<<(ostream &cout,const vector<typC> &a) { int n=a.size(); if (!n) return cout; cout<<a[0]; for (int i=1; i<n; i++) cout<<' '<<a[i]; return cout; }
-// ===================================END Of the input module ==========================================
+#define sz(v) (int)v.size()
+const int mod = 1e9+7;
+const int inf  = 1e18;
 
 
 
-void solve() {
+int32_t main () {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    auto solve = [&] (){
+        
+    };
     
-}
-
-
-int32_t main()
-{
- 
-//freopen("input.txt", "r", stdin);
-//freopen("output.txt", "w", stdout)
- ios_base::sync_with_stdio(false);
- cin.tie(NULL);
-
-    int T = 1;
-    cin >> T;
-    while (T--)
-    {
+    int t=1;
+    cin>>t;
+    while(t--){
         solve();
     }
-    return 0;
+    
 }
